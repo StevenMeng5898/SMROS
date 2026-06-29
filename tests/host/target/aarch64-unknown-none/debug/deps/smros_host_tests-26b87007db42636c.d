bf8671770b6baa91132312d8723c8b95538cb97d
steven meng <neumay@126.com>
2026-06-29 11:50:02 +0800

    git rm tests

--- tests/host/target/aarch64-unknown-none/debug/deps/smros_host_tests-26b87007db42636c.d
+++
@@ -1,5 +0,0 @@
-/home/steven/workspace/SMROS/tests/host/target/aarch64-unknown-none/debug/deps/smros_host_tests-26b87007db42636c.d: src/lib.rs
-
-/home/steven/workspace/SMROS/tests/host/target/aarch64-unknown-none/debug/deps/smros_host_tests-26b87007db42636c: src/lib.rs
-
-src/lib.rs:
